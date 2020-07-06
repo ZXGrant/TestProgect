6c834cefb3d513553b384940a04da3a5ef2841c9
ZXGrant <zxg056339@126.com>
2020-07-06 15:10:52 +0800

    7.6号提交

---
+++ 1001-JavaAPI-test/src/string_test/String1.java
@@ -0,0 +1,16 @@
+/**
+ * 版权所有 (c) 2019，zxg
+ */
+package string_test;
+
+/**
+ * 信息说明
+ * <pre>
+ * Modify Information:
+ * Author          Date          Description
+ * ============   ============= ============================
+ * ZhangXingguang  2020/7/1       Create
+ * </pre>
+ */
+public class String1 {
+}
